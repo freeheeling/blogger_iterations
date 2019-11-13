9be2673ae4c498cdbe70ffac5f54ced742acea68
Scott Regenthal <scottregenthal@gmail.com>
2019-11-12 21:06:05 -0700

    Add feature spec to edit an article

---
+++ spec/features/user_edits_an_article_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'user edits an article' do
+  describe 'they link from an article show page' do
+    describe 'they modify the title and/or body' do
+      it 'displays the updated information on the article show page' do
+        article = Article.create!(title: 'Title 1', body: 'Body 1')
+
+        visit article_path(article)
+
+        click_link 'Edit'
+
+        fill_in 'article[title]', with: 'Updated Title!'
+
+        click_link 'Submit Changes'
+
+        expect(current_path).to eq(article_path(article))
+
+        expect(page).to have_content('Updated Title!')
+        expect(page).to have_content(article.body)
+        expect(page).to_not have_content(article.title)
+      end
+    end
+  end
+end
