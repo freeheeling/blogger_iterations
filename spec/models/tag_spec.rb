1d8fc3809a3294291d948c35e382181093bfaf39
Scott Regenthal <scottregenthal@gmail.com>
2019-11-13 21:23:22 -0700

    Create new article and model methods to return tag names

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,14 @@ describe Tag, type: :model do
     it {should have_many(:taggings)}
     it {should have_many(:articles).through(:taggings)}
   end
+
+  describe 'instance methods' do
+    describe 'to_s' do
+      it 'returns the name of the tag' do
+        tag = Tag.create(name: 'Name')
+
+        expect(tag.to_s).to eq('Name')
+      end
+    end
+  end
 end
